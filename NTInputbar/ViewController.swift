ca8519143ba89eb9565b2cfa562ff87543c10a70
Nicholas Tau <hi@inico.me>
2016-03-28 14:58:18 +0800

    Initial Commit

---
+++ NTInputbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NTInputbar
+//
+//  Created by Nicholas Tau on 3/28/16.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
