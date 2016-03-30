e45994dbf428f20724b09d389ceb9fc148e94df0
Nicholas Tau <hi@inico.me>
2016-03-30 19:05:28 +0800

    BABFrameObservingInputAccessoryView rewrite in swift.

---
+++ NTInputbar/BABFrameObservingInputAccessoryView.swift
@@ -0,0 +1,67 @@
+//
+//  ObserverView.swift
+//  NTInputbar
+//
+//  Created by Nicholas Tau on 3/30/16.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import UIKit
+
+class BABFrameObservingInputAccessoryView: UIView {
+    var inputAccessoryViewFramwChanged :(CGRect -> Void)?
+    var observerAdded = false
+    let BABFrameObservingContext: UnsafeMutablePointer<Void> = nil
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commitInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        commitInit()
+    }
+    
+    private func commitInit(){
+        userInteractionEnabled = false
+    }
+    
+    deinit{
+        if observerAdded{
+            self.superview?.removeObserver(self, forKeyPath:"frame", context: BABFrameObservingContext)
+            self.superview?.removeObserver(self, forKeyPath: "center", context: BABFrameObservingContext)
+        }
+    }
+    
+    func inputAcesssorySuperviewFrame() -> CGRect {
+        return (self.superview?.frame)!
+    }
+    
+    override func willMoveToSuperview(newSuperview: UIView?) {
+        if observerAdded {
+            self.superview?.removeObserver(self, forKeyPath: "frame", context: BABFrameObservingContext)
+            self.superview?.removeObserver(self, forKeyPath: "center", context: BABFrameObservingContext)
+        }
+        newSuperview?.addObserver(self, forKeyPath: "frame", options: .New, context: BABFrameObservingContext)
+        newSuperview?.addObserver(self, forKeyPath: "center", options: .New, context: BABFrameObservingContext)
+        observerAdded = true
+        superview?.willMoveToSuperview(newSuperview)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if object as? UIView == self.superview && (keyPath == "frame" || keyPath == "center"){
+            if self.inputAccessoryViewFramwChanged != nil {
+                self.inputAccessoryViewFramwChanged?((self.superview?.frame)!)
+            }
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if self.inputAccessoryViewFramwChanged != nil {
+            self.inputAccessoryViewFramwChanged?((self.superview?.frame)!)
+        }
+    }
+}
+
