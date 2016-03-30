4fb66b8eceed58e7296485327793431aa722e9d9
Nicholas Tau <hi@inico.me>
2016-03-30 12:32:22 +0800

    add files.

---
+++ NTInputbar/NTInputbarPanel.swift
@@ -0,0 +1,21 @@
+//
+//  NTInputbarPanel.swift
+//  NTInputbar
+//
+//  Created by Nicholas Tau on 3/30/16.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import UIKit
+
+class NTInputbarPanel: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
