75d46aa1ea039569faaaf0c440996830ee1d3afa
Nicholas Tau <hi@inico.me>
2016-03-31 16:34:51 +0800

    add files.

---
+++ NTInputbar/NTInputbarTrigger.swift
@@ -0,0 +1,21 @@
+//
+//  NTInputbarTrigger.swift
+//  NTGrowInputbar
+//
+//  Created by Nicholas Tau on 3/31/16.
+//  Copyright © 2016 JJ. All rights reserved.
+//
+
+import UIKit
+
+class NTInputbarTrigger: NTResponseButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
